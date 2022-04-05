dc15e92161ff5a151d6b2c18d226aaa2e8a7c7ff
Danielle Lemay <Danielle.Lemay@usda.gov>
2022-04-05 11:08:00 -0700

    Update subset_reads.sh

--- subset_reads.sh
+++ subset_reads.sh
@@ -5,9 +5,6 @@ read -p "Enter input directory: " indir
 # Enter output directory
 read -p "Enter output directory: " outdir
 
-#/share/lemaylab-backedup/Zeya/programs/seqtk/seqtk/seqtk sample -s100 $starting_files_location/step_1_BMTagger_output/5052.R1_nohuman.fastq 1000 > $starting_files_location/step_1_BMTagger_output/5052.R1_nohuman_1000reads.fastq
-#/share/lemaylab-backedup/Zeya/programs/seqtk/seqtk/seqtk sample -s100 $starting_files_location/step_1_BMTagger_output/5052.R2_nohuman.fastq 1000 > $starting_files_location/step_1_BMTagger_output/5052.R2_nohuman_1000reads.fastq
- 
 
 # or used the head command
 for file in $indir/500*.fastq 
